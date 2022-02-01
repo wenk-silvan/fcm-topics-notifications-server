8b746f49f7d1451036adaf191097feabc49dc2a9
Silvan Wenk <silvan.wenk@stud.hslu.ch>
2022-02-01 14:56:25 +0100

    Publish simple messages to FCM topics

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TestingNotificationsServer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var defaultApp = FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "key.json")),
+            });
+            Console.WriteLine(defaultApp.Name); // "[DEFAULT]"
+
+            var message = new Message()
+            {
+                Data = new Dictionary<string, string>()
+                {
+                    ["FirstName"] = "Silvan",
+                    ["LastName"] = "Wenk"
+                },
+                Notification = new Notification
+                {
+                    Title = "Test From Server",
+                    Body = "Hello World"
+                },
+                Topic = "project-gamma"
+            };
+
+            var messaging = FirebaseMessaging.DefaultInstance;
+            var result = await messaging.SendAsync(message);
+            Console.WriteLine(result);
+        }
+    }
+}
